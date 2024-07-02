7a9e3ccc55c2c56e1817128d7b55b238e1fb8b64
IsadoraAlcantara <isadoraalca405@gmail.com>
2024-07-02 09:17:16 -0300

    começo da tabela veiculo

---
+++ core/models/veiculo.py
@@ -0,0 +1,16 @@
+from django.db import models
+
+from .modelo import Modelo
+from .cor import Cor
+from .categoria import Categoria
+from .marca import Marca
+
+
+class Veiculo(models.Model):
+    modelo = models.ForeignKey(Modelo, on_delete=models.PROTECT, related_name="veiculos", null=True, blank=True)
+    cor = models.ForeignKey(Cor, on_delete=models.PROTECT, related_name="veiculos", null=True, blank=True)
+    ano = models.IntegerField(null=True, blank=True, default=0)
+    preco = models.DecimalField(max_digits=10, decimal_places=2, default=0, null=True, blank=True)
+
+    def __str__(self):
+        return f"{self.nome} ({self.marca})"
