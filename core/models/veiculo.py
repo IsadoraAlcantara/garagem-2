29799fc9ec7175bee9ba65a160548690c5559afb
IsadoraAlcantara <isadoraalca405@gmail.com>
2024-07-05 08:17:33 -0300

    finalização da API veiculo

--- core/models/veiculo.py
+++ core/models/veiculo.py
@@ -4,6 +4,7 @@ from .modelo import Modelo
 from .cor import Cor
 from .categoria import Categoria
 from .marca import Marca
+from .acessorio import Acessorio
 
 
 class Veiculo(models.Model):
@@ -11,6 +12,7 @@ class Veiculo(models.Model):
     cor = models.ForeignKey(Cor, on_delete=models.PROTECT, related_name="veiculos", null=True, blank=True)
     ano = models.IntegerField(null=True, blank=True, default=0)
     preco = models.DecimalField(max_digits=10, decimal_places=2, default=0, null=True, blank=True)
+    acessorios = models.ForeignKey(Acessorio, on_delete=models.PROTECT, related_name="veiculos", null=True, blank=True)
 
     def __str__(self):
-        return f"{self.nome} ({self.marca})"
+        return f"{self.modelo} {self.ano} {self.cor}"
