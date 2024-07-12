7c9af1ff5b63fd1bca9afa4400490bcba13c24f8
IsadoraAlcanta <Isadoraalca405@gmail.com>
2024-07-12 08:46:14 -0300

    ajuste no models marca

--- core/models/marca.py
+++ core/models/marca.py
@@ -3,7 +3,7 @@ from django.db import models
 
 class Marca(models.Model):
     nome = models.CharField(max_length=50)
-    nacionalicade = models.CharField(max_length=50, blank=True, null=True)
+    nacionalidade = models.CharField(max_length=50, blank=True, null=True)
 
     def __str__(self):
         return f"{self.nome.upper()} ({self.id})"
